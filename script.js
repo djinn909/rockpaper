54afab14fdd2c7d61aa7730ed6e1553f43f7b22c
Djinn <dorian.chenard@gmail.com>
2022-01-10 18:36:23 +0100

    added game (5 rounds) function

--- script.js
+++ script.js
@@ -1,33 +1,60 @@
 
+let computerCount=0; 
+let playerCount=0;
+
+
+
+
 const myArray = ["Rock", "Paper", "Scissors"];
 let playerSelection = "Rock";
 
 
 function computerPlay() {
-  return myArray[~~(Math.random() * myArray.length)];}
+  return myArray[~~(Math.random() * myArray.length)];
+}
 
   
 
   computerSelection = computerPlay().toLowerCase();
   playerSelection = playerSelection.toLowerCase();
 
- function playRound(playerSelection, computerSelection) {
+  
+  
+  
+  function playRound(playerSelection, computerSelection) {
     let result;
-    
     
     if (((playerSelection == "rock") && (computerSelection == "paper"))
     || ((playerSelection == "paper") && (computerSelection == "scissors"))
     || ((playerSelection == "scissors") && (computerSelection == "rock"))) {
-    return result = "You lose ! " + computerSelection + " beats " + playerSelection
+    computerCount++;
+    return result= "You lose ! " + computerSelection + " beats " + playerSelection;
     } else if (playerSelection == computerSelection) {
     return result = "Equality ! Play again !"
     } else {
-      return result = "You win ! " + playerSelection + " beats " + computerSelection }
+      playerCount++; 
+      return result= "You win ! " + playerSelection + " beats " + computerSelection;
+      
+    }}
 
+
+  function game() {
+  for (let round = 0; round < 5; round++) {
+
+    let finalResult;
+    let playerSelection = prompt('Type: Rock, Paper, or Scissors')
+    playerSelection = playerSelection.toLowerCase();
+    const computerSelection = computerPlay().toLowerCase();
+    console.log(playRound(playerSelection, computerSelection)) 
   }
-  
 
-
-  
-  console.log(computerSelection);
-  console.log(playRound(playerSelection, computerSelection));
+    if (playerCount < computerCount) {
+      return finalResult = "You lose ! Computer wins " + computerCount + "-" + playerCount;
+    } else if (playerCount > computerCount) {
+      return finalResult = "You win against the computer !" + playerCount + "-" + computerCount;
+    } else {
+      return finalResult= "Equality ! Play another game !"
+    }
+   
+  }
+  console.log(game(playerCount,computerCount))
